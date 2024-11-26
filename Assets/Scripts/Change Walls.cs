26fb828bac0297fb275df243e52fcbb50dcc5b0a
Not-Bean <alexander.bean@ontariotechu.net>
2024-11-26 15:11:30 -0500

    auto wall detection done

--- Assets/Scripts/Change Walls.cs
+++ Assets/Scripts/Change Walls.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChangeWalls : MonoBehaviour
@@ -7,7 +8,7 @@ public class ChangeWalls : MonoBehaviour
     public Material defaultMaterial;
     public Material selectedMaterial;
     public GameObject[] wall;
-
+    public bool swapped;
     public MeshRenderer[] _object;
     // Start is called before the first frame update
     void Start()
@@ -21,10 +22,28 @@ public class ChangeWalls : MonoBehaviour
 
     public void ChangeWall()
     {
-        
-        for (int j = 0; j < wall.Length; j++)
+        if (swapped == false)
         {
-            wall[j].gameObject.GetComponent<MeshRenderer>().material = _object[j].material.name.StartsWith(defaultMaterial.name) ? selectedMaterial : defaultMaterial;
+            for (int j = 0; j < wall.Length; j++)
+            {
+                //wall[j].gameObject.GetComponent<MeshRenderer>().material = _object[j].material.name.StartsWith(defaultMaterial.name) ? selectedMaterial : defaultMaterial;
+                wall[j].GetComponent<Renderer>().material = defaultMaterial;
+                print(wall[j] + " changed material");
+            }
+
+            swapped = true;
+        }
+
+        else
+        {
+            for (int j = 0; j < wall.Length; j++)
+            {
+                //wall[j].gameObject.GetComponent<MeshRenderer>().material = _object[j].material.name.StartsWith(defaultMaterial.name) ? selectedMaterial : defaultMaterial;
+                wall[j].GetComponent<Renderer>().material = selectedMaterial;
+                print(wall[j] + " changed material");
+            }
+
+            swapped = false;
         }
     }
 }
