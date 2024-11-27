354bebc1a6d4159c4b83b31c1124cd17f64db35a
Not-Bean <alexander.bean@ontariotechu.net>
2024-11-27 01:12:06 -0500

    +pixelation, interpreted texture toggle +readme notes

--- Assets/Read Me.cs
+++ Assets/Read Me.cs
@@ -14,5 +14,19 @@ The LUT was edited to create a toon feel to the game, since toon shaders don't w
 
 
 COURSE PROJECT CHANGES
+
 Revised player shader: changed to make the player have a toon feel
+Added default textured material. reasons for each colour:
+   rock: stone-like colour, makes the game have a natural setting
+   sand: sand, similar colour to what you would find on a beach
+   player: high contrasting colour so it's easy to see on the sand/rock textures
+   chest: colour is used for glow, made for the chest to pop-out from the surroundings
+moving textures: the sand and the water in level 3 has a moving texture on it. both have it to simulate the feel of wind, blowing the sand across the ground and small waves in the water
+waves: as mention before the wind feel adds to the natural feel of the game, adding waves to the water adds to it further
+particles: added explosion when interacting with a chest, this is mostly for fun and feedback for collecting it
+
+
+sand texture: https://assetstore.unity.com/packages/2d/textures-materials/floors/yughues-free-sand-materials-12964
+water texture: https://assetstore.unity.com/packages/2d/textures-materials/water/stylize-water-texture-153577
+particle effects: https://assetstore.unity.com/packages/vfx/particles/legacy-particle-pack-73777
 */
